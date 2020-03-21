51ebcf7ccb320bf6d165f6470c019373f7533f0b
yindaheng98 <yindaheng98@163.com>
2020-03-21 16:37:50 +0800

    Listener option done.

--- option/Option.go
+++ option/Option.go
@@ -1,15 +1,32 @@
 package option
 
-import "github.com/yindaheng98/gogisnet/grpc/server"
+import (
+	"github.com/yindaheng98/gogisnet/grpc/option"
+	"github.com/yindaheng98/gogisnet/grpc/server"
+)
 
 type Option struct {
 	PressureMeterConfig PressureMeterConfig `yaml:"PressureMeterConfig" usage:"Option for PressureMeter."`
 	GogisnetOption      server.Option       `yaml:"GogisnetOption" usage:"Option for gogisnet."`
+	ListenerOption      ListenerOption      `yaml:"ListenerOption" usage:"Option for port listen."`
 }
 
 func DefaultOption() Option {
 	return Option{
 		PressureMeterConfig: DefaultPressureMeterConfig(),
 		GogisnetOption:      server.DefaultOption(),
+		ListenerOption:      defaultListenerOption(),
+	}
+}
+
+type ListenerOption struct {
+	GogisnetListenerOption  option.ListenerOption `yaml:"GogisnetListenerOption" usage:"Listener option for gogisnet."`
+	PressureMeterListenAddr string                `yaml:"PressureMeterListenAddr" usage:"Listen address of the iris server in PressureMeter."`
+}
+
+func defaultListenerOption() ListenerOption {
+	return ListenerOption{
+		GogisnetListenerOption:  option.DefaultListenerOption(),
+		PressureMeterListenAddr: ":80",
 	}
 }
